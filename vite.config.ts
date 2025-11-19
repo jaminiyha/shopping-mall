fd2750325430e95ce0323db018f08c97e925f624
jaminiyha <jaminiyha@gmail.com>
2025-11-19 21:57:24 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,30 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import ViteFaviconsPlugin from "vite-plugin-favicon2"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteFaviconsPlugin({
+      logo: "./public/emoji.png",
+      inject: true,
+      favicons: {
+        appName: "Shopping Mall",
+        appDescription: "온라인 쇼핑몰",
+        developerName: "Shopping Mall",
+        developerURL: null,
+        background: "#fff",
+        theme_color: "#fff",
+        icons: {
+          coast: false,
+          yandex: false,
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
